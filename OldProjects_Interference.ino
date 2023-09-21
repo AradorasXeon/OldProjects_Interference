ccf8832260c072c5e3bcf4e069c46e3b6f9f778f
Krisztián József Varga <aradorasxeon@gmail.com>
2023-09-21 22:46:39 +0200

    as was

---
+++ OldProjects_Interference.ino
@@ -0,0 +1,31 @@
+//https://www.tutorialspoint.com/arduino/arduino_tone_library.htm
+//JUST FOR TESTING
+
+#include "pitches.h"
+// notes in the melody:
+int melody[] = {
+NOTE_C4, NOTE_G3,NOTE_G3, NOTE_GS3, NOTE_G3,0, NOTE_B3, NOTE_C4};
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+
+int noteDurations[] = {
+   4, 8, 8, 4,4,4,4,4 
+};
+
+void setup() {
+   // iterate over the notes of the melody:
+   for (int thisNote = 0; thisNote < 8; thisNote++) {
+      // to calculate the note duration, take one second
+      // divided by the note type.
+      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+      int noteDuration = 1000/noteDurations[thisNote];
+      tone(3, melody[thisNote],noteDuration);
+      
+      //pause for the note's duration plus 30 ms:
+      delay(noteDuration +30);
+   }
+}
+
+void loop() {
+   // no need to repeat the melody.
+   tone(3, 20,10000);
+}
